32f4790367241fd2caea0f8e08d443e5ed26d6ad
bubleti01 <peter-mayers@p2mail.eu>
2026-03-01 21:55:29 +0100

    Update plugin header metadata to version 1.0.1

---
+++ wp-offen-urlaub-post.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WP Offen Urlaub Post
+Plugin URI: https://igo2web.com/de/wordpress-plugins-von-igw-design/wp-offen-urlaub-post
+Description: Erstellen/Verwalten Sie Urlaubszeiten Posts in WordPress und zeigen Sie diese in vielen verschiedenen Widgets und Shortcodes an.
+Version: 1.0.1
+Author: IGW Design
+Author URI: https://igo2web.com
+Text Domain: urlaub_post
+Domain Path: /language
+*/
